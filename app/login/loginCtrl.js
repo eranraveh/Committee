ecce74f065c6a29a7398fc408d5580ea8d627a61
Eran Raveh <xerave1@hotmail.com>
2019-03-05 23:34:56 +0200

    login and messages stab

--- app/login/loginCtrl.js
+++ app/login/loginCtrl.js
@@ -1,9 +1,20 @@
-committeeApp.controller("loginCtrl", function($scope, $location) {
+committeeApp.controller("loginCtrl", function($scope, $location, userSrv) {
 
     $scope.invalidLogin = false;
 
+    $scope.email = "eran@eran.com";
+    $scope.pwd = "123";
+
     $scope.login = function() {
+        // userSrv.login($scope.email, $scope.pwd).then(function(user) {
+        //     $location.path("/myCommittee" +  user.committeeId + "/messages");
+
+        // }, function(error) {
+        //     $scope.invalidLogin = true;
+        // });
         if ($scope.email === "eran@eran.com" && $scope.pwd === "123") {
+            // $stage.go();
+            // $('#loginForm').modal('hide');
             $location.path("/myCommittee1/messages");
         } else {
             $scope.invalidLogin = true;
---
+++ app/messages/messagesCtrl.js
@@ -0,0 +1,5 @@
+committeeApp.controller("messagesCtrl", function($scope, $location) {
+
+$scope.test = "11111";
+
+})
